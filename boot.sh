6ff4395226029d783aca52a972c644ff38bad342
Yaman <eliver.com12@gmail.com>
2025-10-25 15:55:29 +0400

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ansi_art='
-██╗   ██╗ ██████╗ ██╗  ████████╗
-██║   ██║██╔═══██╗██║  ╚══██╔══╝
-██║   ██║██║   ██║██║     ██║   
-╚██╗ ██╔╝██║   ██║██║     ██║   
- ╚████╔╝ ╚██████╔╝███████╗██║   
-  ╚═══╝   ╚═════╝ ╚══════╝╚═╝   
+██╗   ██╗██╗██╗      ██████╗ 
+██║   ██║██║██║     ██╔═══██╗
+██║   ██║██║██║     ██║   ██║
+╚██╗ ██╔╝██║██║     ██║   ██║
+ ╚████╔╝ ██║███████╗╚██████╔╝
+  ╚═══╝  ╚═╝╚══════╝ ╚═════╝ 
                                  
 ⚡ Powered by Ractor Package Manager
 React Apps at Lightning Speed
@@ -27,21 +27,21 @@ wget https://raw.githubusercontent.com/CyberHuman-bot/Ractor/refs/heads/main/rac
 chmod +x ractor.sh
 sudo mv ractor.sh /usr/local/bin/ractor
 
-# Use custom repo if specified, otherwise default to Milicop/Volt
-VOLT_REPO="${VOLT_REPO:-Milicop/Volt}"
+# Use custom repo if specified, otherwise default to CyberHuman-bot/Vilo
+VILO_REPO="${VILO_REPO:-CyberHuman-bot/Vilo}"
 
-echo -e "\n⚡ Cloning Volt from: https://github.com/${VOLT_REPO}.git"
-rm -rf ~/.local/share/volt/
-git clone "https://github.com/${VOLT_REPO}.git" ~/.local/share/volt >/dev/null
+echo -e "\n⚡ Cloning Vilo from: https://github.com/${VILO_REPO}.git"
+rm -rf ~/.local/share/vilo/
+git clone "https://github.com/${VILO_REPO}.git" ~/.local/share/vilo >/dev/null
 
 # Use custom branch if instructed, otherwise default to master
-VOLT_REF="${VOLT_REF:-master}"
-if [[ $VOLT_REF != "master" ]]; then
-  echo -e "\e[32m⚡ Using branch: $VOLT_REF\e[0m"
-  cd ~/.local/share/volt
-  git fetch origin "${VOLT_REF}" && git checkout "${VOLT_REF}"
+VILO_REF="${VILO_REF:-master}"
+if [[ $VILO_REF != "master" ]]; then
+  echo -e "\e[32m⚡ Using branch: $VILO_REF\e[0m"
+  cd ~/.local/share/vilo
+  git fetch origin "${VILO_REF}" && git checkout "${VILO_REF}"
   cd -
 fi
 
 echo -e "\n⚡ Installation starting..."
-source ~/.local/share/volt/install.sh
+source ~/.local/share/vilo/install.sh

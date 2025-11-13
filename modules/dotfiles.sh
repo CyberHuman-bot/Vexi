5fe27a0dc73dc94f048f98d99116dab7ac56a5b8
Yaman <eliver.com12@gmail.com>
2025-11-13 17:35:45 +0400

    Update dotfiles.sh

--- modules/dotfiles.sh
+++ modules/dotfiles.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 apply_dotfiles() {
-    gum spin --title "Applying dotfiles and themes..." -- \
+    gum spin --title "Applying Vilo dotfiles and Powerlevel10k theme..." -- \
         git clone https://github.com/CyberHuman-bot/Vilo-dotfiles.git "$HOME/.vilo-dotfiles" \
         && cp -r "$HOME/.vilo-dotfiles/." "$HOME/" \
-        && git clone https://github.com/adi1090x/termux-ohmyzsh.git "$HOME/.vilo-themes"
+        && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended \
+        && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" \
+        && sed -i 's|^ZSH_THEME=.*|ZSH_THEME="powerlevel10k/powerlevel10k"|' "$HOME/.zshrc" \
+        && echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> "$HOME/.zshrc" \
+        && echo "source ~/.zshrc" > "$HOME/.profile"
 }
